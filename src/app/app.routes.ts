1b0bf4d2a7337c66266353994af9b0bebe679f89
SajjadBayat <sajjad.bayat32@gmail.com>
2025-02-14 21:37:09 +0330

    feat: implement app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { MainListPageComponent } from './pages/main-list-page/main-list-page.component';
+import { CompletedListPageComponent } from './pages/completed-list-page/completed-list-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // {
+  //   path: 'main',
+  //   component: MainListPageComponent,
+  // },
+  {
+    path: 'done',
+    component: CompletedListPageComponent,
+  },
+  {
+    path: 'list',
+    component: ListPageComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'main',
+    pathMatch: 'full', // Ensures the default route goes to 'main'
+  },
+  {
+    path: '**',
+    redirectTo: 'main', // Wildcard for unknown routes
+  },
+];
